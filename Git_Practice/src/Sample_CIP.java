61f80c1e6b3d675880e182a47fa60db030244450
Manikandangk <manikandangokul83@gmail.com>
2021-10-03 14:47:20 +0530

    Signed-off-by: Manikandangk <manikandangokul83@gmail.com>

---
+++ Git_Practice/src/Sample_CIP.java
@@ -0,0 +1,13 @@
+
+public class Sample_CIP {
+
+	public static void main(String[] args) {
+	
+		String a="Contineous";
+		String b="Integration";
+		String c="Process";		
+		System.out.println(a+b+c);
+
+	}
+
+}
